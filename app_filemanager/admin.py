4e1988387b9fbcb32e72e3da3f3b04c4eeeae37c
Yuan gao <nauy2011@126.com>
2015-02-01 20:03:06 +0800

    1.修改上传界面的tip显示位置；2.修改admin添加tag的时候user自动获取并隐藏选择框。

--- app_filemanager/admin.py
+++ app_filemanager/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import Tag, File, Project
 # Register your models here.
 
-admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("owner", )
+        form = super(TagAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+admin.site.register(Tag, TagAdmin)
 admin.site.register(File)
 admin.site.register(Project)
