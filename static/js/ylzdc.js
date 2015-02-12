99112fcf1d5b74b49a5092e53dd67847a9c6cbe6
Yuan Gao <nauy2011@126.com>
2015-02-12 11:15:50 +0800

    jquery

--- static/js/ylzdc.js
+++ static/js/ylzdc.js
@@ -1,8 +1,25 @@
-// JavaScript Document
+// 当点击M键的时候，class增加一个"open"
 $(document).keypress(function(event){
   if(event.which === 109){
     $('.dropdown').toggleClass('open');
   };
 })
-// 当点击M键的时候，class增加一个"open"
-$(document).ready(pressm);
+
+$(document).keypress(function(event){
+  if(event.which === 110){
+    $('.left_space').toggle(50);
+    $('.right_space').toggle(50);
+    $('.middle_space').toggleClass('col-md-8');
+  };
+})
+//var main = function() {
+//    $('.article').click(function() {
+//        $('.article').removeClass('current');
+//        $('.description').hide();
+//        $(this).addClass('current');
+//        $(this).children('.description').show();
+//    });
+//};
+
+//$(document).ready(main);
+//$(document).ready(pressm);
